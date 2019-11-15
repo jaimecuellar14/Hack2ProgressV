fd99f30cd55fbbc6ca4d2e8ce54075dbcfee41d6
Juan Cardona <juan.cardona@alumnos.unealtantico.es>
2019-11-15 22:40:31 +0100

    Intro - Falta logo

--- cilantriyo/src/app/intro/intro.page.ts
+++ cilantriyo/src/app/intro/intro.page.ts
@@ -7,11 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IntroPage implements OnInit {
   slideOpts = {
-    initialSlide: 1,
+    initialSlide: 0,
     speed: 400
   };
   constructor() { }
-
+  
   ngOnInit() {
   }
 
